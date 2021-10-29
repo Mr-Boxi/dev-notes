475e70beab302dbf2cf83621ebcfb5b6895e5aa9
zhangzhanwen <2909027418@qq.com>
2021-10-29 11:12:47 +0800

    update

--- 区块链/智能合约开发/solidity语法/solidity例子/SimpleAuction.sol
+++ 区块链/智能合约开发/solidity语法/solidity例子/SimpleAuction.sol
@@ -2,7 +2,13 @@ pragma solidity ^0.4.0;
 
 // 简单的拍卖
 contract SimpleAuction {
-    function SimpleAuction(){
 
-    }
+    address public beneficiary;
+    uint public auctionEnd;
+
+    address public highestBidder;
+    uint public highestBid;
+
+    mapping(address => uint) pendingReturnes;
+
 }
